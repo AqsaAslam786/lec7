4e5923be38a788ec47ee2278ef83f6e9217440a2
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-09-21 14:21:25 +0500

    chan

--- my_project/src/App.jsx
+++ my_project/src/App.jsx
@@ -1,26 +1,38 @@
 import ProfileCard from "./components/ProfileCard"
-import profile_card from "./components/ProfileCard"
-import TestCaed from "./components/TestCaed"
 
 function App() {
 
+const user1={
+  name:"Aqsa Aslam",
+  title:"Web Developer",
+  pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&s",
+
+
+};
+const user2={
+  name:"Talan Daop",
+  title:"UX Designer",
+  pic:"https://a.storyblok.com/f/191576/1176x882/f95162c213/profile_picture_hero_before.webp",
+
+
+};
+const user3={
+  name:"Hadia Ali",
+  title:"UI Designer",
+  pic:"https://a.storyblok.com/f/191576/1176x882/f95162c213/profile_picture_hero_before.webp",
+
+
+};
 
   return (
     <>
-    <div className="flex justify-center items-center">
-<div className="flex justify-center items-center h-screen">
     
-       
-        <ProfileCard name="Aqsa Aslam" title="Web Developer" pic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&s"    />
-        
+<div className="flex flex-col md:flex-row  lg:flex-col h-screen gap-2">
+        <ProfileCard user={user1} title="Heading 1"  />
+      <ProfileCard user={user2} title="Heading 2"  />
+      <ProfileCard user={user3} title="Heading 3"  />
       </div>
-      <div className="flex justify-center items-center h-screen">
-    
-       
-        <ProfileCard name="Talan Dias" title="UX Designer" pic="https://a.storyblok.com/f/191576/1176x882/f95162c213/profile_picture_hero_before.webp"    />
-        
-      </div>
-    </div>
+      
       
     </>
   )
--- my_project/src/components/ProfileCard.jsx
+++ my_project/src/components/ProfileCard.jsx
@@ -1,20 +1,20 @@
 
 import React from 'react'
 
-export default function ProfileCard({name,title,pic}) {
+export default function ProfileCard({ user }) {
   return (
-    <div className='bg-white shadow-xl/30 w-200 h-100 rounded-4xl flex justify-center items-center'>
+    <div className='bg-white shadow-xl/30 w-160 h-80 rounded-4xl flex justify-center items-center'>
       
       <div>
-          <img className='size-52 rounded-full mr-12' src={pic} alt="" />
-        </div>
-
-        <div className='mb-8'>
-          <h1 className='text-3xl font-bold'>{name}</h1>
-          <h2 className='text-2xl '>{title.toUpperCase()}</h2>
-        </div>
+        <img className='size-52 rounded-full mr-12' src={user.pic} alt="" />
       </div>
-      
-  
+
+      <div className='mb-8'>
+        <h1 className='text-3xl font-bold'>{user.name}</h1>
+        <h2 className='text-2xl '>{user.title?.toUpperCase() ?? "UX/UI Designer"}</h2>
+      </div>
+    </div>
+
+
   )
 }
